e026171367ac37a721e1e971877deb5f7269a68a
fadl <fadlhashim666@gmail.com>
2025-05-05 12:28:19 +0500

    i removed the / in url

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,22 @@
 from app import app
+from flask import render_template,flash,redirect,url_for
+from app.forms import LoginForm
+
+@app.route('/login', methods= ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+
+
+
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hellow world!"
+    user = {"username" :  "kira" }
+    return render_template('index.html',title="home", user=user )
+    
